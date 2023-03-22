8f817179c9c701553d1f763ca4db3b8b9b9ed1b4
zen900220 <59685788+zen900220@users.noreply.github.com>
2023-03-22 18:56:42 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        console.log(1);
+        elements.forEach((ele) => ele.classList.remove("hidden"));
+      } else {
+        console.log(2);
+        elements.forEach((ele) => ele.classList.add("hidden"));
+      }
+    });
+  },
+  {
+    threshold: 0.8,
+    root: null,
+  }
+);
+
+const elements = document.querySelectorAll(".hidden");
+
+// elements.forEach((element) => observer.observe(element));
+observer.observe(document.querySelector(".intro"));
